f6a585dcb176083ae59a87eaaa48eec16b9a7eb9
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-09-28 08:59:22 +0100

    first commit

---
+++ Source/WallBreaker/WallBreakerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WallBreakerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WALLBREAKER_API AWallBreakerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
